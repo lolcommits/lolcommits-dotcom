acf62e62d37f601f584b12a0751e2a92ed8cda8f
Matthew Hutchinson <matt@hiddenloop.com>
2018-03-26 21:15:25 +0100

    remove runner_order, bump gem version

--- lib/lolcommits/dotcom/version.rb
+++ lib/lolcommits/dotcom/version.rb
@@ -1,5 +1,5 @@
 module Lolcommits
   module Dotcom
-    VERSION = "0.0.5".freeze
+    VERSION = "0.1.0".freeze
   end
 end
--- test/lolcommits/plugin/dotcom_test.rb
+++ test/lolcommits/plugin/dotcom_test.rb
@@ -6,10 +6,6 @@ describe Lolcommits::Plugin::Dotcom do
   include Lolcommits::TestHelpers::GitRepo
   include Lolcommits::TestHelpers::FakeIO
 
-  it "should run on capture ready" do
-    ::Lolcommits::Plugin::Dotcom.runner_order.must_equal [:capture_ready]
-  end
-
   describe "with a runner" do
     def runner
       # a simple lolcommits runner with an empty configuration Hash
